7f8fa3cc7defca0e68de7384d3cdcc00c75ce266
Helder Matos <mtshelder@gmail.com>
2021-05-26 00:35:48 -0300

    Adding File Handling class implementation

--- src/file_handling.py
+++ src/file_handling.py
@@ -1,4 +1,23 @@
-
+from pathlib import Path
 
 class FileHandling():
-    pass
+    def __init__(self, path_to_file: str):
+        
+        self.absolute_filename = Path(path_to_file)
+        
+        if self.absolute_filename.exists():
+            self.file = open(self.absolute_filename)
+        else:
+            raise ValueError("Invalid path to file")
+    
+    def print(self):
+        print(self.absolute_filename)
+    
+    def get_raw_input(self):
+        dict_input = dict()
+        lines = self.file.readlines()
+        
+        dict_input["Components"] = lines[0][:-1]
+        dict_input["Rules"] = [line[:-1] for line in lines[1:]]
+        
+        return dict_input
